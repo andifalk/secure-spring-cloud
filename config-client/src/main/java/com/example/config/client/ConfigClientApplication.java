0bf1919720a4c0226fc6c6578c96b0dae7bf144a
Andreas Falk <andreas.falk@novatec-gmbh.de>
2017-02-26 02:06:30 +0100

    add service resgistry

--- config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/config/client/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.example.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
